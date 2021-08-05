66b50484051b6c229bea1b0606adc6f3cfb1e16e
iva1111 <mvg75@mail.ru>
2021-08-05 14:41:58 +0500

    add correction error

--- src/test/java/ru/netology/StatisticsServiceTest.java
+++ src/test/java/ru/netology/StatisticsServiceTest.java
@@ -6,15 +6,11 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class StatisticsServiceTest {
 
-   @Test
-   void findMax() {
-       StatisticsService service = new StatisticsService();
+    @Test
+    void findMax() {
+        StatisticsService service = new StatisticsService();
 
-       long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-       long expected = 12;
-
-       long actual = service.findMax(incomesInBillions);
-
-      assertEquals(expected, actual);
-   }
+        int[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12, 15};
+        assertEquals(15, service.findMax(incomesInBillions));
+    }
 }
